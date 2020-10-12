5d5b90b149939756b0e5776c532cd98e618972de
PerRL <leikange@gmail.com>
2020-10-12 21:32:46 +0200

    dimentionality(case)

--- src/Samsara.jl
+++ src/Samsara.jl
@@ -8,6 +8,17 @@ module Samsara
 abstract type AbstractSystem end
 
 struct MockSystem <: AbstractSystem
+    _system_state
+    function MockSystem(system_parameters=())
+        new(system_parameters)
+    end
 end
 
+#### Functions ###
+function dimentionality(sys::AbstractSystem)
+    length(sys._system_state)
+end
+
+
+export dimentionality           #EXPORT
 end # module
--- test/test_Samsara.jl
+++ test/test_Samsara.jl
@@ -4,6 +4,11 @@ using Samsara, Test
 @testset "system initiation" begin
     case = Samsara.MockSystem()
     @test isa(case, Samsara.AbstractSystem)
+
+    @test isa(case._system_state, Tuple)
+
+    @test length(case._system_state) == 0
+    @test dimentionality(case) == 0
 end
 
 
