55c0b529ebcdde9748316ca098def530f8bfa34b
PerRL <leikange@gmail.com>
2020-10-12 22:10:14 +0200

    Can now supply a system_mechanics function lambda as named arg. to ctor

--- src/Samsara.jl
+++ src/Samsara.jl
@@ -8,17 +8,41 @@ module Samsara
 abstract type AbstractSystem end
 
 struct MockSystem <: AbstractSystem
-    _system_state
-    function MockSystem(system_parameters=())
-        new(system_parameters)
+    _system_dynamics_callback
+    _system_parameters
+    function MockSystem(;system_dynamics=()->(nothing, ))
+        new(system_dynamics, system_dynamics())
     end
 end
 
 #### Functions ###
-function dimentionality(sys::AbstractSystem)
-    length(sys._system_state)
+" system_state(sys::AbstractSystem) returns system state of sys "
+function system_state(sys::AbstractSystem)
+    sys._system_parameters
 end
 
+" dimentionality(sys::AbstractSystem) returns dimentionality "
+function dimentionality(sys::AbstractSystem)
+    if isnothing(system_state(sys)[1]) 
+        return 0
+    else
+        length(system_state(sys))
+    end
+end
 
-export dimentionality           #EXPORT
+""" _update_system_simulation(sys::AbstractSystem)
+Update _system_parameters from _system_dynamics_callback.
+"""
+function _update_system_simulation!(sys::AbstractSystem)
+    _system_parameters = sys._system_dynamics_callback()
+end
+
+# PLAN
+# - Etter kvart step, oppdateres _system_parameters. 
+#       step(sys, event) => simuler, så skriv til _system_parameters inne i step
+# - _system_parameters kan hentes ut ved observe(AbstractSystem)
+# - 
+
+
+export dimentionality, system_state           #EXPORT
 end # module
--- test/test_Samsara.jl
+++ test/test_Samsara.jl
@@ -5,10 +5,17 @@ using Samsara, Test
     case = Samsara.MockSystem()
     @test isa(case, Samsara.AbstractSystem)
 
-    @test isa(case._system_state, Tuple)
+    @test isa(system_state(case), Tuple)
 
-    @test length(case._system_state) == 0
+    # System state is nothing, since no system_dynamics was supplied in ctor 
+    @test system_state(case) == (nothing, )
     @test dimentionality(case) == 0
+
+    # 1D system mechnics: Supplied λ-function returns a scalar.
+    case_1D = Samsara.MockSystem(system_dynamics=()->(1.0))
+    @test dimentionality(case_1D) == 1
+    Samsara._update_system_simulation!(case_1D)
+    @test system_state(case_1D) == (1.0)
 end
 
 
