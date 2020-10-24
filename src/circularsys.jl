e2a5fd0acff2546337c40dce1099c62790554a1b
PerRL <leikange@gmail.com>
2020-10-24 11:51:59 +0200

    Hahaha, glømte å legge til filene: CircularSys

---
+++ src/circularsys.jl
@@ -0,0 +1,43 @@
+using Samsara
+
+mutable struct CircularSys <: Samsara.AbstractSystem
+    _all_nodes
+    _current_index::Int
+    function CircularSys(number_of_nodes =3)
+        _all_nodes = zeros(number_of_nodes)
+        new(_all_nodes, 1)
+    end
+end
+
+function system_state(sys::CircularSys)
+    [sys._current_index]
+end
+current_index(sys::CircularSys) = system_state(sys)[1]
+
+" Dispatch Base.length for this struct: "
+function Base.length(sys::CircularSys)
+    length(sys._all_nodes)
+end
+
+""" 
+function step_up!(sys::CircularSys)
+Step one up. 
+Return next, i.e. index of stepping one step "up".
+When at max-position, go around to beginning 
+"""
+function step_up!(sys::CircularSys)
+    sys._current_index == length(sys) && (return sys._current_index = 1)
+    sys._current_index+1
+end
+
+""" 
+function step_down!(sys::CircularSys)
+Step one down. 
+Return next index, i.e. index of stepping one step "down"
+When at min-position, go around to the end
+"""
+function step_down!(sys::CircularSys)
+    #sys._current_index == length(sys) && (return sys._current_index = 1)
+    sys._current_index-1
+end
+
---
+++ test/test_circularsys.jl
@@ -0,0 +1,35 @@
+module TEST_CIRULARSYS
+using Samsara, Test
+
+@testset "circularsys initiation" begin
+    CircularSys = Samsara.CircularSys
+    current_index = Samsara.current_index
+    step_up! = Samsara.step_up!
+    step_down! = Samsara.step_down!
+
+    case = CircularSys()
+    @test isa(case, AbstractSystem)
+
+    # default antall noder: 3
+    @test length(case._all_nodes) == 3
+    @test length(CircularSys(2)._all_nodes) == 2
+    #@test length(case) == 2
+
+    # current state: The index of current location
+    case = CircularSys(3)
+    @test system_state(case) == [1]
+    @test step_up!(case) == 2
+    " step_up! steps one 'up' "
+
+    case._current_index = 3
+    @test current_index(case) == 3
+    @test step_up!(case) == 1
+    @test current_index(case) == 1
+    " step_up! steps around to beginning when rounding the end => Circular! "
+
+    case._current_index = 2
+    @test step_down!(case) == 1
+
+end
+
+end#module TEST_CIRULARSYS
