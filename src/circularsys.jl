fce219387435fbf2f47143fe3af72d173d8e152f
PerRL <leikange@gmail.com>
2020-10-24 12:22:56 +0200

    restructure: Gjorde circular system bedre egna for å integreres inn i Samsara for neoRL

--- src/circularsys.jl
+++ src/circularsys.jl
@@ -3,16 +3,26 @@ using Samsara
 mutable struct CircularSys <: Samsara.AbstractSystem
     _all_nodes
     _current_index::Int
-    function CircularSys(number_of_nodes =3)
-        _all_nodes = zeros(number_of_nodes)
+    function CircularSys(nodes =[:A, :B, :C])
+        _all_nodes = nodes
         new(_all_nodes, 1)
     end
 end
 
+"""
+system_state(sys::CircularSys) 
+returnerer full system state for CircularSys -- a Tuple of observable_param 
+"""
 function system_state(sys::CircularSys)
-    [sys._current_index]
+    (sys._all_nodes[sys._current_index], )
 end
-current_index(sys::CircularSys) = system_state(sys)[1]
+
+""" 
+current_state(sys::CircularSys)
+Returns the current state, where we are currently located, only.
+"""
+current_state(sys::CircularSys) = sys._all_nodes[current_index(sys)]
+current_index(sys::CircularSys) = sys._current_index
 
 " Dispatch Base.length for this struct: "
 function Base.length(sys::CircularSys)
@@ -25,7 +35,7 @@ Step one up.
 Return next, i.e. index of stepping one step "up".
 When at max-position, go around to beginning 
 """
-function step_up!(sys::CircularSys)
+function _step_up!(sys::CircularSys)
     sys._current_index == length(sys) && (return sys._current_index = 1)
     sys._current_index+=1
 end
@@ -36,7 +46,7 @@ Step one down.
 Return next index, i.e. index of stepping one step "down"
 When at min-position, go around to the end
 """
-function step_down!(sys::CircularSys)
+function _step_down!(sys::CircularSys)
     sys._current_index == 1 && (return sys._current_index = length(sys))
     sys._current_index-=1
 end
--- test/test_circularsys.jl
+++ test/test_circularsys.jl
@@ -4,20 +4,24 @@ using Samsara, Test
 @testset "circularsys initiation" begin
     CircularSys = Samsara.CircularSys
     current_index = Samsara.current_index
-    step_up! = Samsara.step_up!
-    step_down! = Samsara.step_down!
+    current_state = Samsara.current_state
+    step_up! = Samsara._step_up!
+    step_down! = Samsara._step_down!
 
     case = CircularSys()
     @test isa(case, AbstractSystem)
 
-    # default antall noder: 3
+    # default antall noder: 3 zeroes
     @test length(case._all_nodes) == 3
-    @test length(CircularSys(2)._all_nodes) == 2
-    #@test length(case) == 2
+    @test length(case) == 3
+    @test case._all_nodes[1] == :A  # default list of nodes: [:A, :B, :C]
 
-    # current state: The index of current location
-    case = CircularSys(3)
-    @test system_state(case) == [1]
+    # current state: The current node in a Tuple
+    case = CircularSys()
+    @test system_state(case) == (:A, )
+    " system_state(case) returns the value of the current node "
+
+    case._current_index = 1
     @test step_up!(case) == 2
     " step_up! steps one 'up' "
 
@@ -27,7 +31,7 @@ using Samsara, Test
     @test current_index(case) == 1
     " step_up! steps around to beginning when rounding the end => Circular! "
 
-    case = CircularSys(4)
+    case = CircularSys([1:4;])
     @test current_index(case) == 1
     @test step_up!(case) == 2
     @test step_up!(case) == 3
@@ -35,12 +39,18 @@ using Samsara, Test
     @test step_up!(case) == 1
     " integration test: Step up works for a full circle "
 
-    case = CircularSys(2)
+    case = CircularSys([:state1, :state2])
     case._current_index = 2
     @test step_down!(case) == 1
     step_down!(case)
     @test current_index(case) == 2
+    @test system_state(case) == (:state2, )
+    @test current_state(case) == :state2
     " Full round integration test for step_down! (on length(case) == 2) "
+end
+
+@testset "System testing: circular system with deterministic actions" begin
+    
 end
 
 end#module TEST_CIRULARSYS
