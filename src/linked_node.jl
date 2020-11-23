68643e14499cb9a376826850a7e72bb54b2c2cdd
Per RL <leikange@gmail.com>
2020-11-23 19:58:27 +0100

    la til node W og E, med default=nothing

--- src/linked_node.jl
+++ src/linked_node.jl
@@ -12,10 +12,16 @@ Each link is of type Conception.TemporalType (like SAT):
 struct LinkedNode <: Conception.TemporalType
     _id
     _member_of_MuEx
-    function LinkedNode(;in_MuEx::Union{AbstractMuExS, Nothing}=nothing)
+    _node_E::Union{Conception.Conception.TemporalType, Nothing}
+    _node_W::Union{Conception.Conception.TemporalType, Nothing}
+    function LinkedNode(;
+                        node_to_E =nothing,
+                        node_to_W =nothing,
+                        in_MuEx::Union{AbstractMuExS, Nothing}=nothing)
         set_of_MuExS = AbstractMuExS[]
         !isnothing(in_MuEx) && push!(set_of_MuExS, in_MuEx)
-        the_node = new(:id, set_of_MuExS)
+        # Init
+        the_node = new(:id, set_of_MuExS, node_to_E, node_to_W)
 
         Conception.add_element_to_MuExS!(in_MuEx, the_node)
 
--- test/test_linked_node.jl
+++ test/test_linked_node.jl
@@ -2,15 +2,19 @@ module TEST_LINKED_NODE
 using Samsara, Conception, Test
 
 @testset "LinkNode initiation" begin
-    case = Samsara.LinkedNode()
-    @test isa(case, Samsara.LinkedNode)
-    @test isa(case, Conception.AbstractConcept)
+    caseNode = Samsara.LinkedNode()
+    @test isa(caseNode, Samsara.LinkedNode)
+    @test isa(caseNode, Conception.AbstractConcept)
     " LinkedNode lager en struct av type Conception.AbstractConcept --- som også SAT gjør... "
 
     state_set = Conception.MuExS()
-    case = Samsara.LinkedNode(in_MuEx=state_set)
-    @test Conception.exists_in_MuExS(case, state_set)
+    caseNode = Samsara.LinkedNode(in_MuEx=state_set)
+    @test Conception.exists_in_MuExS(caseNode, state_set)
     " Lage en LinkedNode => registreres i arg: [in_MuEx] "
+
+    @test isnothing(caseNode._node_W)
+    @test isnothing(caseNode._node_E)
+    #@test isa(caseNode._node_W, Conception.AbstractConcept)
 end
 
 
