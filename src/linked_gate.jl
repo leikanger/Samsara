9548369339a6379d5887d417ef101b55463492e6
Per R.L <leikange@gmail.com>
2020-12-11 14:11:21 +0100

    gate is by default closed, but can be opened by open_gate!(the-gate)

--- src/linked_gate.jl
+++ src/linked_gate.jl
@@ -7,11 +7,28 @@ LinkedGate is a subtype of Conception.AbstractConcept to mark that it's not supp
 mutable struct LinkedGate <: Conception.AbstractConcept
     pre_node
     post_node
+    _gate_is_open
     function LinkedGate(nodeA::T, nodeB::T) where {T <: Conception.AbstractConcept}
-        new(nodeA, nodeB)
+        new(nodeA, nodeB, false)
     end
 end
 
+""" is_open(LinkedGate) 
+getter for whether gate is open or not """
+function gate_is_open(the_gate::LinkedGate)
+    the_gate._gate_is_open
+end
+
+""" open_gate(the_gate)
+Open up the gate, such that activation of gate causes transmission from nodeA to nodeB or vice versa 
+"""
+function open_gate!(the_gate::LinkedGate)
+    the_gate._gate_is_open = true
+end
 
 
-
+""" activate!(LinkedGate)   => OPEN the gate
+activate port, i.e. open the gate
+(Extends Conception.activate!(..) -- but not TemporalType, since AbstractConcept is higher. 
+Therefore, I have to explicitly write Conception.activate! in the next sentence)
+"""
--- test/test_linked_gate.jl
+++ test/test_linked_gate.jl
@@ -4,16 +4,21 @@ using Samsara, Conception, Test
 
 @testset "LinkedGate" begin
     n1, n2 = SAT(:preNode), SAT(:postNode)
+    case = LinkedGate(n1, n2)
+
     @test isa(LinkedGate(n1, n2), Conception.AbstractConcept)
     """ Definere plass for LinkedGate mtp. typehierarki """
 
-    case = LinkedGate(n1, n2)
     @test case.pre_node == n1
     @test case.post_node == n2
     """ We can create a linked gate with pre-node and post-node by ctor """
 
-    #@test is_open(case) == false 
-    #""" default value for a gate is 'closed' """
+    @test Samsara.gate_is_open(case) == false 
+    """ default value for a gate is 'closed' """
+
+    Samsara.open_gate!(case)
+    @test Samsara.gate_is_open(case) == true
+    """ after "activating" the gate, i.e. opening it, the gate is open """
 
     # When a gate:closed is activated, we "are" still at pre-node.
     # When a gate:open is activated, we move to post-node.
